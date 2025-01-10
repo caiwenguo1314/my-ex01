34dfcac054ce5a0dceca67da98cd9a9a79931493
caiwenguo <328668100@qq.com>
2025-01-10 16:49:28 +0800

    临时修改

---
+++ src/Componets/Layout/AppContent4th/AppContent4th.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AppContent4th() {
+  return (
+    <div>
+      Review
+    </div>
+  )
+}
--- src/Componets/Layout/AppLayout.js
+++ src/Componets/Layout/AppLayout.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Layout } from "antd";
+import { useState } from "react";
 
 import AppHeader from "./AppHeader/AppHeader";
 import AppSteps from "./AppSteps/AppSteps";
-import AppContent1th from "./AppContent1th/AppContent1th";
+
 import AppFooter from "./AppFooter/AppFooter";
+import AppContent1th from "./AppContent1th/AppContent1th";
+import AppContent2th from "./AppContent2th/AppContent2th";
 import AppContent3th from "./AppContent3th/AppContent3th";
+import AppContent4th from "./AppContent4th/AppContent4th";
+
+
+
+
 
 const AppLayout = () => {
+
+  const [ step, setStept ] = useState(1);
+  console.log('step:', step);
   return (
     <Layout
       style={{
@@ -24,8 +35,10 @@ const AppLayout = () => {
 
       {/* Content */}
 
-      {/* <AppContent1th /> */}
-      <AppContent3th />
+      {step === 1 && <AppContent1th />}
+      {step === 2 && <AppContent2th />}
+      {step === 3 && <AppContent3th />}
+      {step === 4 && <AppContent4th />}
 
       {/* Footer */}
       <AppFooter />
